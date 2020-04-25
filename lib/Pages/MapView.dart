950d63096da3150f625e61bb30245718eb4a0595
Jimmy Kang <jimmykang1016@gmail.com>
2020-04-25 15:23:44 -0700

    make MapView functional

--- lib/Pages/MapView.dart
+++ lib/Pages/MapView.dart
@@ -4,16 +4,38 @@ import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
-  
+  Map locationsToDisplay = Map();
+
+  MapView({this.locationsToDisplay});
   @override
-  _MapViewState createState() => _MapViewState();
+  _MapViewState createState() => _MapViewState(
+    locationsToDisplay: locationsToDisplay,
+  );
 }
 
 class _MapViewState extends State<MapView> {
+  Completer<GoogleMapController> _controller = Completer();
+  Map locationsToDisplay = Map();
+  
+  _MapViewState({this.locationsToDisplay});
+
+  static final CameraPosition _defaultStart = CameraPosition(
+    target: LatLng(33.693139, -117.789026),
+    bearing: 0, 
+    tilt: 0,
+    zoom: 12,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: blue,
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _defaultStart,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      )
     );
   }
 }
